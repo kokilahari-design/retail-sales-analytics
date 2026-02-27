e92d274db3cd536505ee52bd73ac963af7017ff9
kokilahari-design <kokila2065@gmail.com>
2026-02-27 11:49:44 +0530

    Add comments to Flask app initialization
    Added comments explaining the Flask application setup.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+# Flask is a main class that creates your web application/ API application.
+# Object of Flask class is our WSGI application. Flask constructor takes the name of current module (__name__) as argument.
+
 
 from flask import Flask
 
