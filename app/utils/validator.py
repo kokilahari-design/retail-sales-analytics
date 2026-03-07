8d0b54b7bde207c59046f666ae1c4535fd5a98dc
kokilahari-design <kokila2065@gmail.com>
2026-03-08 02:34:31 +0530

    Implement request validation for sale data
    Added a utility function to validate incoming sale data against required fields.

--- app/utils/validator.py
+++ app/utils/validator.py
@@ -1,1 +1,13 @@
+# Request Validation Utility : data validation step
+    # checks if incoming JSON data from client contains all the necessary key fields.
 
+
+REQUIRED_FIELDS = [ "customer_id", "customer_name", "product_id", "product_name", "product_category", "quantity", "price", "sale_datetime" ]
+
+def validate_sale_data(data):
+
+    for field in REQUIRED_FIELDS:
+        if field not in data:
+            return False, f"Missing field {field}"
+
+    return True, "Valid"
