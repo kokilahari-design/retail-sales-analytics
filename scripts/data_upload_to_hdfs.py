a60b301e0d05154e65264dba1465908a6daa5099
kokilahari-design <kokila2065@gmail.com>
2026-03-12 21:05:16 +0530

    Updated data_upload_to_hdfs.py

---
+++ scripts/data_upload_to_hdfs.py
@@ -0,0 +1,18 @@
+import subprocess
+import logging
+from data_genrator import generate_daily_sales_csv
+
+logging.basicConfig(level=logging.INFO)
+
+def upload_to_hdfs(local_file):
+
+    hdfs_dir = "/data/retail/raw/"
+    try:
+        command = ["hdfs", "dfs", "-put", "-f", local_file, hdfs_dir]
+        subprocess.run(command, check=True)
+        logging.info(f"{local_file} uploaded to HDFS {hdfs_dir}")
+        return True
+    except subprocess.CalledProcessError as e:
+        logging.error("HDFS upload failed")
+        raise e
+
