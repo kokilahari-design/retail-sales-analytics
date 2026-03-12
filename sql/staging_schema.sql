4aa3510b73c9ff6aa4dee9637e83758e36a45cc3
kokilahari-design <kokila2065@gmail.com>
2026-03-12 21:23:29 +0530

    Create staging_schema.sql

---
+++ sql/staging_schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE sales_staging
+(
+customer_id INT,
+customer_name VARCHAR(100),
+product_id INT,
+product_name VARCHAR(100),
+product_category VARCHAR(100),
+price DECIMAL(10,2),
+quantity INT,
+sale_datetime TIMESTAMP
+);
