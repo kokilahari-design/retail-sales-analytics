342ab33e584c75fa78a3544f6f20973db195c951
kokilahari-design <kokila2065@gmail.com>
2026-03-12 20:09:00 +0530

    Update test_event_producer.py

--- scripts/test_event_producer.py
+++ scripts/test_event_producer.py
@@ -7,7 +7,7 @@ from scripts.data_generator import generate_sale_event
 API_URL = "http://127.0.0.1:5000/testing"
 
 for _ in range(5):
-    data =  generate_daily_sales_csv()
+    data =  upload_to_hdfs()
     response = requests.post(API_URL, json=data)   # sends HTTP POST request to the API endpoint server with the generated sale data as JSON payload
     print(response.json())
    
