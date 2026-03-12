44c4b0f993a7656c27811a24a50de1b1d6867b84
kokilahari-design <kokila2065@gmail.com>
2026-03-12 20:09:24 +0530

    Update upload_to_hdfs.py

--- scripts/upload_to_hdfs.py
+++ scripts/upload_to_hdfs.py
@@ -1,9 +1,9 @@
 import subprocess
-from data_genrator import generate_daily_sales
+from data_genrator import generate_daily_sales_csv
 
 def upload_to_hdfs():
 
-    local_file = generate_daily_sales()
+    local_file = generate_daily_sales_csv()
     hdfs_dir = "/data/retail/raw/"
     command = f"hdfs dfs -put -f {local_file} {hdfs_dir}"
     subprocess.run(command, shell=True, check=True)
