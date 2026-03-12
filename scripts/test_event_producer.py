499a752c8d69da2bcfbf7b27c70482d7f5ff301e
kokilahari-design <kokila2065@gmail.com>
2026-03-12 20:38:28 +0530

    Update test_event_producer.py

--- scripts/test_event_producer.py
+++ scripts/test_event_producer.py
@@ -2,12 +2,12 @@
 # requests is to send HTTP requests (like GET, POST, PUT, DELETE) and work with APIs easily.
 
 import requests
-from scripts.data_generator import generate_sale_event
+from scripts.upload_to_hdfs import uploaded_to_hdfs
 
 API_URL = "http://127.0.0.1:5000/testing"
 
 for _ in range(5):
-    data =  upload_to_hdfs()
-    response = requests.post(API_URL, json=data)   # sends HTTP POST request to the API endpoint server with the generated sale data as JSON payload
+    extract_data =  uploaded_to_hdfs()
+    response = requests.post(API_URL, json=extract_data)   # sends HTTP POST request to the API endpoint server with the generated sale data as JSON payload
     print(response.json())
    
